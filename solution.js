616579146c2f84322b177fff2ee4dda5af74a09d
xstartstaticx <daniel.reiller@icloud.com>
2022-06-29 16:45:04 +0200

    project finished

--- solution.js
+++ solution.js
@@ -1,1 +1,20 @@
+const str = "Danielson";
+console.log(str);
 
+console.log(str[1], str[5]);
+
+let firstName = "Danica";
+let age = "28";
+let year = "2022";
+console.log(firstName, "is", age, "in", year);
+
+let isMarried = false;
+console.log(firstName, "is married:", isMarried);
+
+let programmingLanguage = "JavaScript";
+let isFun = true;
+console.log(programmingLanguage, "is fun", true);
+
+console.log(programmingLanguage[0], programmingLanguage[4]);
+
+console.log(programmingLanguage.length);
